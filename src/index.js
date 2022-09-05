09ecd5ba93c1f4d6ab985b924472569262b66bfe
Anoop Jadhav <anoop.jadhav@bp.com>
2022-09-05 11:17:30 +0530

    [Anoop] Update react version to 18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App/App";
 import Store from "./components/Store/Store";
 import { Provider } from "react-redux";
+
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>
 );
