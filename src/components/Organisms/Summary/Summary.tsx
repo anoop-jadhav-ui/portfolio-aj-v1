ae9cb307a2f20abe50731322f9486d6521dc87c8
Anoop Jadhav <anoop.jadhav@bp.com>
2022-09-07 21:41:49 +0530

    [Anoop] profile image dark mode changes

--- src/components/Organisms/Summary/Summary.tsx
+++ src/components/Organisms/Summary/Summary.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import myImage from "../../../assets/AnoopJadhav.png";
+import lightImage from "../../../assets/portfolio-app-img-light-min.png";
+import darkImage from "../../../assets/portfolio-app-img-dark-min.png";
 import "./Summary.css";
 import Loader from "../../Atoms/Loader/Loader";
 import { useGlobalContext } from "../../../context/GlobalContext";
+import { useTheme } from "../../../context/ThemeContext";
 
 const Summary = () => {
   const [imageLoading, setImageLoading] = useState(false);
   const { profileData } = useGlobalContext();
+  const { darkMode } = useTheme();
   const { overview } = profileData;
   function handleImageLoaded() {
     setImageLoading(true);
@@ -21,7 +24,7 @@ const Summary = () => {
       <div className="col-md-7 page-1 text-center ">
         <div className="mainlogo-wrapper">
           <img
-            src={myImage}
+            src={darkMode ? darkImage : lightImage}
             className={`mainlogo ${imageLoading && "loaded"}`}
             alt="mi Baburao"
             loading="eager"
