07f9d6128b7284ac1aada182d84032e781b5a80b
Anoop Jadhav <anoop.jadhav@bp.com>
2022-09-15 11:47:26 +0530

    [Anoop] cleaned css from common Css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App/App";
 import Providers from "./helpers/Providers";
 import "./CommonStyles.scss";
+import "./BootstrapOverrideStyles.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
