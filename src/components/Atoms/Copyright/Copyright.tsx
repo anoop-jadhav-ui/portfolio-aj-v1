aa16b628f5b1f1082e4e9de7cbc23e6b34fc45d2
Anoop Jadhav <anoopjadhav@gmail.com>
2023-04-27 00:04:22 +0530

    minor ui improvments

--- src/components/Atoms/Copyright/Copyright.tsx
+++ src/components/Atoms/Copyright/Copyright.tsx
@@ -3,11 +3,7 @@ import "./Copyright.scss";
 export default function Copyright() {
   return (
     <div className="copyright">
-      <span>
-        All rights reserved <span className="primary-color">©</span> Anoop
-        Jadhav
-      </span>
-      <span>{` ${new Date().getFullYear()}`}</span>
+      <span>Copyright &copy; {new Date().getFullYear()} Anoop Jadhav</span>
     </div>
   );
 }
