5aa733737105211eb82d5421c1a0285e3dc0981a
Anoop Jadhav <anoopjadhav@gmail.com>
2023-04-29 00:33:55 +0530

    add vercel web analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import App from "./App/App";
 import Providers from "./helpers/Providers";
 import "./CommonStyles.scss";
 import "./BootstrapOverrideStyles.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <Providers>
     <App />
+    <Analytics />
   </Providers>
 );
