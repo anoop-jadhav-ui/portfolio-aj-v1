d89dbb5595e599331c0ca08e3fc18864b435dbf4
Anoop Jadhav <anoopjadhav@gmail.com>
2023-06-30 18:09:06 +0530

    message form updated

--- src/components/Screens/MessageForm/MessageForm.tsx
+++ src/components/Screens/MessageForm/MessageForm.tsx
@@ -1,7 +1,7 @@
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { RiMailSendLine } from "react-icons/ri";
-import { useProfileDataContext } from "../../../context/ProfileDataContext";
 import axiosInstance from "../../../helpers/axios";
 import constants from "../../../helpers/constants";
 import T from "../../../translations/en_IN";
@@ -10,18 +10,31 @@ import Button from "../../Atoms/Button/Button";
 import SectionWrapper from "../../Organisms/SectionWrapper/SectionWrapper";
 import "./MessageForm.scss";
 
+type MessageFormType = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const defaultMessageFormState: MessageFormType = {
+  name: "",
+  email: "",
+  message: "",
+};
+
 const MessageForm = () => {
-  const [message, updateMessage] = useState<string>("");
-  const [name, updateName] = useState<string>("");
+  const methods = useForm<MessageFormType>({
+    defaultValues: defaultMessageFormState,
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = methods;
 
   const [bannerStatus, updateBannerStatus] = useState<BannerStatus>("neutral");
   const [showBanner, setShowBanner] = useState(false);
-
-  const {
-    profileData: { contactDetails },
-  } = useProfileDataContext();
-
-  const { emailId } = contactDetails;
 
   const getBannerMessage = {
     success: T.MESSAGE_SENT_SUCCESS,
@@ -29,36 +42,25 @@ const MessageForm = () => {
     neutral: T.SENDING_MESSAGE,
   };
 
-  function onMessageInputChange(e: React.FormEvent<HTMLTextAreaElement>) {
-    updateMessage((e.target as HTMLTextAreaElement).value);
-  }
-  function onNameInputChange(e: React.FormEvent<HTMLInputElement>) {
-    updateName((e.target as HTMLInputElement).value);
-  }
+  const successHandler: SubmitHandler<MessageFormType> = async (data) => {
+    const { name, email, message } = data;
+    try {
+      setShowBanner(true);
+      updateBannerStatus("neutral");
+      const response = await axiosInstance.post("/mail", {
+        email: email,
+        message: message,
+        name: name,
+      });
 
-  const sendEmail = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (message) {
-      try {
-        setShowBanner(true);
-        updateBannerStatus("neutral");
-        const response = await axiosInstance.post("/mail", {
-          userEmail: emailId,
-          userMessage: message,
-          userName: name,
-        });
-
-        if (response.data.msg === "success") {
-          updateBannerStatus("success");
-
-          updateMessage("");
-          updateName("");
-        } else if (response.data.msg === "fail") {
-          updateBannerStatus("error");
-        }
-      } catch (e: unknown) {
+      if (response.data.msg === "success") {
+        updateBannerStatus("success");
+        reset();
+      } else if (response.data.msg === "fail") {
         updateBannerStatus("error");
       }
+    } catch (e: unknown) {
+      updateBannerStatus("error");
     }
   };
 
@@ -79,31 +81,73 @@ const MessageForm = () => {
       <form
         id="contact-form"
         className="subsection message-content"
-        onSubmit={(event) => sendEmail(event)}
-        method="POST"
+        onSubmit={handleSubmit(successHandler)}
+        noValidate
       >
         <div className="subsection-data">
           <p className="subsection-title body-text">
             {T.MESSAGE_FORM_SUBTITLE}
           </p>
-          <div className="primary-color body-text">
-            <input
-              id="name"
-              value={name}
-              placeholder="Your Name"
-              name="name"
-              onChange={onNameInputChange}
-              required
-            />
-            <textarea
-              id="message-box"
-              value={message}
-              className=""
-              placeholder="Your Message"
-              name="message"
-              onChange={onMessageInputChange}
-              required
-            />
+          <div className="primary-color body-text form-wrapper">
+            <div className="input-form-control">
+              <label htmlFor="name">Full Name</label>
+              <input
+                {...register("name", {
+                  required: "Please enter your name.",
+                })}
+                placeholder="John Cena"
+                name="name"
+                id="name"
+                className={Boolean(errors.name) ? "highlight-error" : ""}
+              />
+              {errors.name && (
+                <span className="error-message-label">
+                  {errors.name?.message}
+                </span>
+              )}
+            </div>
+            <div className="input-form-control">
+              <label htmlFor="email">Email Address</label>
+              <input
+                {...register("email", {
+                  required: "Email is required",
+                  validate: {
+                    maxLength: (v) =>
+                      v.length <= 50 ||
+                      "Email should have at most 50 characters",
+                    matchPattern: (v) =>
+                      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+                      "Email address must be a valid address",
+                  },
+                })}
+                placeholder="johncena@gmail.com"
+                name="email"
+                type="email"
+                id="email"
+                className={Boolean(errors.email) ? "highlight-error" : ""}
+              />
+              {errors.email && (
+                <span className="error-message-label">
+                  {errors.email?.message}
+                </span>
+              )}
+            </div>
+            <div className="input-form-control">
+              <label htmlFor="message">Message</label>
+              <textarea
+                {...register("message", {
+                  required: "Message is required.",
+                })}
+                placeholder="Tell me more about your project, needs and timeline."
+                name="message"
+                className={Boolean(errors.message) ? "highlight-error" : ""}
+              />
+              {errors.message && (
+                <span className="error-message-label">
+                  {errors.message?.message}
+                </span>
+              )}
+            </div>
           </div>
 
           <Button
