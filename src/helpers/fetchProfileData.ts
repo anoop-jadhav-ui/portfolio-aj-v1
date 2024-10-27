c67f4c9649a6281697322b1604d489528ade5c5d
anoop-jadhav-ui <anoopjadhav@gmail.com>
2024-10-27 22:56:15 +0530

    chore: ignore api call on dev

--- src/helpers/fetchProfileData.ts
+++ src/helpers/fetchProfileData.ts
@@ -1,21 +1,21 @@
-import firebaseApp from './firebaseApp'
-import { getDatabase, ref, child, get } from 'firebase/database'
+import { child, get, getDatabase, ref } from 'firebase/database'
 import testProfileData from '../data/testData.json'
+import firebaseApp from './firebaseApp'
 
 export default async function fetchProfileData() {
     try {
-        // if (import.meta.env.MODE === 'development') {
-        //     console.log('Loading local data. Skipping the api call.')
-        //     return testProfileData
-        // } else {
-        const dbRef = ref(getDatabase(firebaseApp))
-        const snapshot = await get(child(dbRef, '/'))
-        if (snapshot.exists()) {
-            return snapshot.val()
+        if (import.meta.env.MODE === 'development') {
+            console.log('Loading local data. Skipping the api call.')
+            return testProfileData
         } else {
-            throw 'Api call failed.'
+            const dbRef = ref(getDatabase(firebaseApp))
+            const snapshot = await get(child(dbRef, '/'))
+            if (snapshot.exists()) {
+                return snapshot.val()
+            } else {
+                throw 'Api call failed.'
+            }
         }
-        // }
     } catch (err) {
         console.log(err)
         return testProfileData
