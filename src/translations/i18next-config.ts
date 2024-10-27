61c57e59f0fa0ac7974ef4a4a14240dbdf9cdbe5
anoop-jadhav-ui <anoopjadhav@gmail.com>
2024-10-27 12:31:42 +0530

    chore: remove translations

--- src/translations/i18next-config.ts
+++ src/translations/i18next-config.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
-import { deTranslation, enTranslation, hiTranslation } from './translations'
+import { enTranslation } from './translations'
 
 i18n.use(initReactI18next)
     .use(LanguageDetector)
@@ -9,8 +9,6 @@ i18n.use(initReactI18next)
         fallbackLng: ['en'],
         resources: {
             en: { translation: enTranslation },
-            de: { translation: deTranslation },
-            hi: { translation: hiTranslation },
         },
     })
 
--- src/translations/translations.ts
+++ src/translations/translations.ts
@@ -1,7 +1,3 @@
-import de from './de/translation.json'
 import en from './en/translation.json'
-import hi from './hi/translation.json'
 
-export const deTranslation = de
 export const enTranslation = en
-export const hiTranslation = hi
