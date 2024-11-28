d0376a8bacc4183ba0516175e9bd5dc251e96613
anoop-jadhav-ui <anoopjadhav@gmail.com>
2024-11-28 19:54:07 +0530

    chore: lazy load downlaod page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './translations/i18next-config' // This import should be always at the top of the file
 
-import React, { useRef } from 'react'
+import React, { lazy, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import PageScrollProgressBar from 'react-page-scroll-progress-bar'
 import { Route, Routes } from 'react-router'
 import ErrorBoundary from './components/Molecules/ErrorBoundary/ErrorBoundary'
 import { Stars } from './components/Molecules/Stars/Stars'
-import DownloadCV from './components/Pages/DownloadCVPage/DownloadCVPage'
 import LandingPage from './components/Pages/LandingPage/LandingPage'
 import AlertBannerProvider from './context/AlertBannerContext'
+
+const DownloadCVPage = lazy(
+    () => import('./components/Pages/DownloadCVPage/DownloadCVPage')
+)
 
 export const App = () => {
     const { t } = useTranslation()
@@ -26,7 +29,7 @@ export const App = () => {
                 <div className="app-container" ref={AppRef}>
                     <Routes>
                         <Route index element={<LandingPage />} />
-                        <Route path="download" element={<DownloadCV />} />
+                        <Route path="download" element={<DownloadCVPage />} />
                     </Routes>
                 </div>
             </AlertBannerProvider>
